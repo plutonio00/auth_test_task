209cc117f50d1bec27168dcc6d1b1e06500e2bc3
viktor_kozhinov <victor.kozhinov@westwing.ru>
2020-05-09 13:30:41 +0300

    auth: add logic

--- src/core/Database.php
+++ src/core/Database.php
@@ -2,18 +2,40 @@
 
 namespace app\core;
 
+use PDO;
+use PDOException;
+
 class Database
 {
     private $pdo;
 
-    public function __construct()
+    public function __construct($connectionName = 'db')
     {
+        $app = Application::instance();
+
         try {
-            //todo: add connection
-        }
-        catch (\PDOException $e) {
+            $pass = $app->getConfig($connectionName)['pass'];
+            $user = $app->getConfig($connectionName)['user'];
+            $name = $app->getConfig($connectionName)['name'];
+            $host = $app->getConfig($connectionName)['host'];
+            $dsn = sprintf('mysql:dbname=%s;host=%s', $name, $host);
+
+            $this->pdo = new PDO($dsn, $user, $pass);
+        } catch (PDOException $e) {
             echo $e->getMessage();
-            echo "Can't connect to database";
+            echo 'Can\'t connect to database';
+        }
+    }
+
+    public function queryGet(string $sql, array $params = [])
+    {
+        if ($params) {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        else {
+            return $this->pdo->query($sql)->fetchAll();
         }
     }
 }
