1feff812d103fa195c0d00b80977c962b7bf110d
viktor_kozhinov <victor.kozhinov@westwing.ru>
2020-05-09 13:32:02 +0300

    auth: add js handler for forms

---
+++ public/js/form_handler.js
@@ -0,0 +1,45 @@
+
+let authForm = document.querySelector('.js-auth-form');
+
+authForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    resetValidateErrors();
+
+    let form = e.target;
+    let formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(result => {
+            handleValidateErrors(result);
+        });
+});
+
+let handleValidateErrors = function (errors) {
+    for (let key in errors) {
+        let selector = `.js-auth-form #${key}`;
+        let input = document.querySelector(selector);
+        let errorText = document.querySelector(`${selector} + .js-error-text`);
+        input.classList.add('is-invalid');
+        errorText.innerText = errors[key];
+    }
+};
+
+let resetValidateErrors = function () {
+    let inputs = document.querySelectorAll('.js-input');
+    let errors = document.querySelectorAll('.js-error-text');
+
+    inputs.forEach(input => {
+        input.classList.remove('is-invalid');
+    });
+
+    errors.forEach(error => {
+        error.innerText = '';
+    });
+};
