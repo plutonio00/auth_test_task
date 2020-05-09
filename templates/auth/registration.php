f35b2605810b6e5fab418b0417342cd6b28b8075
viktor_kozhinov <victor.kozhinov@westwing.ru>
2020-05-09 13:59:08 +0300

    auth: add registration form

--- templates/auth/registration.php
+++ templates/auth/registration.php
@@ -7,15 +7,26 @@
                 <small class="js-error-text form-text text-danger"></small>
             </div>
             <div class="form-group">
+                <label for="first_name">First name</label>
+                <input type="text" class="js-input form-control" id="first_name" name="first_name" placeholder="Enter first name" required>
+                <small class="js-error-text form-text text-danger"></small>
+            </div>
+            <div class="form-group">
+                <label for="last_name">Last name</label>
+                <input type="text" class="js-input form-control" id="last_name" name="last_name" placeholder="Enter email" required>
+                <small class="js-error-text form-text text-danger"></small>
+            </div>
+            <div class="form-group">
                 <label for="password">Password</label>
                 <input type="password" class="js-input form-control" id="password" name="password" placeholder="Password"
                        required>
                 <small class="js-error-text form-text text-danger"></small>
             </div>
+
             <div class="form-group">
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="remember_me" name="remember_me">
-                    <label class="form-check-label" for="remember_me">Remember me</label>
+                    <input type="checkbox" class="form-check-input" id="agree_terms" name="agree_terms">
+                    <label class="form-check-label" for="agree_terms">I accept the terms of use of the site</label>
                     <small class="js-error-text form-text text-danger"></small>
                 </div>
             </div>
@@ -23,7 +34,7 @@
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
             <div class="form-group">
-                <p>Don't have an account? <a href="/auth/login">Sign in</a></p>
+                <p>Already have an account? <a href="/auth/login">Sign in</a></p>
             </div>
         </form>
     </div>
