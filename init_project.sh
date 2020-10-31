734d92d7eb7a63598a06404065e15bae0994e4e8
vkozhinov <vkozhinov@voximplant.com>
2020-10-31 21:18:52 +0300

    fix bugs in init script

--- init_project.sh
+++ init_project.sh
@@ -1,9 +1,14 @@
 cp env.php.example env.php
+
 mkdir logs
-touch php_errors.log
-sudo chmod 777 -R logs
-sudo chmod 777 -R public/images
-echo "127.0.0.1 auth_task.local" >> ~/hosts-copy
+touch logs/php_errors.log
+
+echo "127.0.0.1 auth_task.local" | sudo tee --append /etc/hosts > /dev/null
+
 cd docker
 cp .env.example .env
-#docker-compose up -d
+docker-compose up -d
+cd ..
+
+sudo chmod 777 -R logs
+sudo chmod 777 -R public/images
