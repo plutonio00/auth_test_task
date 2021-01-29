e499461fb9ff6077597748082ba6d75aa058942b
vkozhinov <vkozhinov@voximplant.com>
2021-01-29 18:36:11 +0300

    ref_and_fixes: some fixes

--- init_project.sh
+++ init_project.sh
@@ -8,7 +8,6 @@ echo "127.0.0.1 auth_task.local" | sudo tee --append /etc/hosts > /dev/null
 cd docker
 cp .env.example .env
 docker-compose up -d
-cd ..
 
-sudo chmod 777 -R logs
-sudo chmod 777 -R public/images
+docker exec auth_task_php chmod 777 -R logs
+docker exec auth_task_php chmod 777 -R public/images
